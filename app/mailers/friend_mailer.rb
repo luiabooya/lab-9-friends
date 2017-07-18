419724501a678f207bb4c34fca9006295c201fbb
Luia Yen <luia.yen@gmail.com>
2017-07-18 15:54:27 -0500

    initial setup of Friend Mailer

---
+++ app/mailers/friend_mailer.rb
@@ -0,0 +1,2 @@
+class FriendMailer < ApplicationMailer
+end
---
+++ config/initializers/setup_mailer.rb
@@ -0,0 +1,8 @@
+ActionMailer::Base.smtp_settings = {  
+    :address              => "smtp.gmail.com",  
+    :port                 => 587,  
+    :user_name            => "xluiax@gmail.com",  
+    :password             => "$$HuJiK9",  
+    :authentication       => "plain",  
+    :enable_starttls_auto => true  
+  }
