cbdba1d2dd2f58ea08dcbf6874662a8d51d17aa5
Luia Yen <luia.yen@gmail.com>
2017-07-18 15:44:02 -0500

    add validations for Friend model

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,12 @@
 class Friend < ApplicationRecord
   
+  validates :name, presence: true
+  validates :phone, format: { with: /^\d{10}|\(?\d{3}\)?[-\. ]?\d{3}[-\. ]?\d{4}$/,
+    message: "Please input a valid phone number; only space, dash, period, and parentheses allowed"}
+  validates :email, presence: true, format: { with: /^([\w\.]+)@[a-zA-Z0-9\.]+\.[a-zA-Z]{2,4}$/,
+    message: "Please input a valid email" }
+  validates :level, presence: true, inclusion: { in: [1,2,3,4], message: "Please select a level from 1-4" }
+  
   FRIENDSHIP_LEVELS = [ ["Casual", 1], ["Good", 2], ["Close", 3], ["Best", 4] ]
   
 end
