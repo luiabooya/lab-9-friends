bb6f3e24b155f0cbe5e07aed2ea01057a08d05d0
Luia Yen <luia.yen@gmail.com>
2017-07-18 17:55:52 -0500

    migration to connect uploader to friend model

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,5 @@
 class Friend < ApplicationRecord
-  
+
   validates :name, presence: true
   validates :phone, presence: true, format: { with: /\A\d{10}|\(?\d{3}\)?[-\. ]?\d{3}[-\. ]?\d{4}\z/,
     message: "Please input a valid phone number; only space, dash, period, and parentheses allowed"}
@@ -8,5 +8,7 @@ class Friend < ApplicationRecord
   validates :level, presence: true, inclusion: { in: [1,2,3,4], message: "Please select a level from 1-4" }
   
   FRIENDSHIP_LEVELS = [ ["Casual", 1], ["Good", 2], ["Close", 3], ["Best", 4] ]
+  
+  mount_uploader :photo, PhotoUploader
   
 end
