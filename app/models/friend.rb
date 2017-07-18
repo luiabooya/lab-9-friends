1048cdc8539674e3768ca5e5836052294679f97e
Luia Yen <luia.yen@gmail.com>
2017-07-18 15:47:40 -0500

    updated the regex lines

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,9 +1,9 @@
 class Friend < ApplicationRecord
   
   validates :name, presence: true
-  validates :phone, format: { with: /^\d{10}|\(?\d{3}\)?[-\. ]?\d{3}[-\. ]?\d{4}$/,
+  validates :phone, presence: true, format: { with: /\A\d{10}|\(?\d{3}\)?[-\. ]?\d{3}[-\. ]?\d{4}\z/,
     message: "Please input a valid phone number; only space, dash, period, and parentheses allowed"}
-  validates :email, presence: true, format: { with: /^([\w\.]+)@[a-zA-Z0-9\.]+\.[a-zA-Z]{2,4}$/,
+  validates :email, presence: true, format: { with: /\A([\w\.]+)@[a-zA-Z0-9\.]+\.[a-zA-Z]{2,4}\z/,
     message: "Please input a valid email" }
   validates :level, presence: true, inclusion: { in: [1,2,3,4], message: "Please select a level from 1-4" }
   
