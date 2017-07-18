0fd01efc9cfe2c6ec626f2d6776c430557dc09b0
Luia Yen <luia.yen@gmail.com>
2017-07-18 16:02:16 -0500

    create methods for new and remove friend

--- app/mailers/friend_mailer.rb
+++ app/mailers/friend_mailer.rb
@@ -1,2 +1,13 @@
 class FriendMailer < ApplicationMailer
+  
+  def new_friend_msg(friend)
+    @friend = friend
+    mail(to: friend.email, subject: "My New Friend")
+  end
+  
+  def remove_friend_msg(friend)
+    @friend = friend
+    mail(to: friend.email. subject: "We Are No Longer Friends")
+  end
+  
 end
